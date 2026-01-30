d0a01376bdbd1839464679901c85f2c41273340d
Sharukh <135739979+Zaira111111@users.noreply.github.com>
2026-01-31 00:49:55 +0530

    Update StableCoin.sol

--- contracts/StableCoin.sol
+++ contracts/StableCoin.sol
@@ -1,2 +1,14 @@
-ffcffffgfehfrggtgfccdfgffggggfhgffcfgffffffygfg
-u
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(msg.sender) {}
+
+    function mint(address _to, uint256 _amount) external onlyOwner returns (bool) {
+        _mint(_to, _amount);
+        return true;
+    }
+}
